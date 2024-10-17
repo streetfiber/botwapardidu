fc1e6b8d99b20410268e89b1f4b864ff0e34cf29
streetfiber <108503295+streetfiber@users.noreply.github.com>
2024-10-17 23:09:52 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-require('dotenv').config();
+require('dotenv').config()
 const Pepesan = require("pepesan");
 const router = require("./router");
 const { ALLOWED_NUMBERS } = process.env;
 
 (async () => {
-    try {
-        const config = {
-            allowedNumbers: ALLOWED_NUMBERS ? ALLOWED_NUMBERS.split(',') : null,
-            browserName: 'Dewakoding App'
-        }
-        
-        const pepesan = Pepesan.init(router, config);
-
-        // Menggunakan register untuk event QR
-        pepesan.register('qr', (qr) => {
-            console.log('QR Code generated:', qr); // Menampilkan QR Code
-        });
-
-        // Menangani koneksi
-        await pepesan.connect();
-        console.log('Connected to WhatsApp.');
-    } catch (error) {
-        console.error('Error connecting to WhatsApp:', error);
+    const config = {
+        allowedNumbers: ALLOWED_NUMBERS ? ALLOWED_NUMBERS.split(',') : null,
+        browserName: 'Dewakoding App'
     }
-})();
+    const pepesan = Pepesan.init(router, config)
+    await pepesan.connect()
+    console.log('QR Code data generated:', );
+})()
