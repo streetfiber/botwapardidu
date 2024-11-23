5d871fb5ea7e2b9f6278e2d5f0ff0dd99d3c053f
streetfiber <108503295+streetfiber@users.noreply.github.com>
2024-11-23 17:02:46 +0700

    commit setup

--- router.js
+++ router.js
@@ -9,6 +9,6 @@ router.menu(f("menu.rincian"), [BotController, "rincian"]);
 router.menu(f("menu.chatadmin"), [BotController, "chatadmin"]);
 router.menu(f("menu.feedback"), [BotController, "feedback"]);
 router.menu(f("menu.helper"), [BotController, "helper"]);
-router.keyword("*", [BotController, "introduction"]);
+router.keyword("/start", [BotController, "introduction"]);
 
 module.exports = router;
